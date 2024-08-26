71f3493ee59b321fa88fba13a6e200830d7ec138
hakueiMac <king20010115@gmail.com>
2024-08-26 16:01:43 +0800

    feat: Update main.css for todo list styling improvements

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,74 +2,28 @@
     <div class="conatiner todoListPage vhContainer">
         <div class="todoList_Content">
             <div class="inputBox">
-                <input type="text" placeholder="請輸入待辦事項">
-                <span class="material-symbols-outlined">add</span>
+                <input type="text" placeholder="請輸入待辦事項" v-model="inputValue">
+                <span class="material-symbols-outlined" @click="addTodo">add</span>
             </div>
             <div class="todoList_list">
-                <!-- <ul class="todoList_tab">
-                        <li><a href="#" class="active">全部</a></li>
-                        <li><a href="#">待完成</a></li>
-                        <li><a href="#">已完成</a></li>  這可以用filter來做
-                    </ul> -->
-                <div class="todoList_items">
+                <ul class="todoList_tab">
+                        <li><span :class="{ active: activeTab === 'all' }" @click="setActiveTab('all')">全部</span></li>
+                        <li><span :class="{ active: activeTab === 'incompleted' }" @click="setActiveTab('incompleted')">待完成</span></li>
+                        <li><span :class="{ active: activeTab === 'completed' }" @click="setActiveTab('completed')">已完成</span></li>
+                </ul>
+                <p class="p_showMessage" v-if="todoList.length === 0">目前尚無待辦事項</p>
+                <div class="todoList_items" v-else>
                     <ul class="todoList_item">
-                        <li>
+                        <li v-for="todo in filteredTodoList" :key="todo.createTime">
                             <label class="todoList_label">
-                                <input class="todoList_input" type="checkbox" value="true">
-                                <span>把冰箱發霉的檸檬拿去丟</span>
+                                <input class="todoList_input" type="checkbox" :checked="todo.status" @change="toggleTodo(todo.id)">
+                                <span>{{ todo.content }}</span>
                             </label>
-                            <a href="#">
-                                <i class="fa fa-times"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <label class="todoList_label">
-                                <input class="todoList_input" type="checkbox" value="true">
-                                <span>打電話叫媽媽匯款給我</span>
-                            </label>
-                            <a href="#">
-                                <i class="fa fa-times"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <label class="todoList_label">
-                                <input class="todoList_input" type="checkbox" value="true">
-                                <span>整理電腦資料夾</span>
-                            </label>
-                            <a href="#">
-                                <i class="fa fa-times"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <label class="todoList_label">
-                                <input class="todoList_input" type="checkbox" value="true">
-                                <span>繳電費水費瓦斯費</span>
-                            </label>
-                            <a href="#">
-                                <i class="fa fa-times"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <label class="todoList_label">
-                                <input class="todoList_input" type="checkbox" value="true">
-                                <span>約vicky禮拜三泡溫泉</span>
-                            </label>
-                            <a href="#">
-                                <i class="fa fa-times"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <label class="todoList_label">
-                                <input class="todoList_input" type="checkbox" value="true">
-                                <span>約ada禮拜四吃晚餐</span>
-                            </label>
-                            <a href="#">
-                                <i class="fa fa-times"></i>
-                            </a>
+                            <span class="material-symbols-outlined" @click="deleteTodo(todo.id)">close</span>
                         </li>
                     </ul>
                     <div class="todoList_statistics">
-                        <p> 5 個已完成項目</p>
+                        <p> {{ finishTodo }} 個已完成項目</p>
                     </div>
                 </div>
             </div>
@@ -77,4 +31,145 @@
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+    import { ref, onMounted, computed } from 'vue';
+    import Swal from 'sweetalert2';
+
+    const inputValue = ref('');
+    const activeTab = ref('all');
+    const api = 'https://todolist-api.hexschool.io/todos';
+
+    const todoList = ref([]);
+
+    const getTodo = async () => {
+        await fetch(api, {
+            method: 'GET',
+            headers: {
+                'Authorization': sessionStorage.getItem('token'),
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then((res) => {
+            todoList.value = res.data;
+        })
+        .catch((err) => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: err.message
+            });
+        });
+    }
+
+    const addTodo = async () => {
+        if (inputValue.value === '') {
+            Swal.fire({
+                icon: 'error',
+                title: 'Empty input',
+                text: 'Please enter a new todo'
+            });
+            return;
+        }
+
+        await fetch(api, {
+            method: 'POST',
+            headers: {
+                'Authorization': sessionStorage.getItem('token'),
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'content': inputValue.value
+            })
+        })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.status) {
+                getTodo();
+                inputValue.value = '';
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Success',
+                    text: 'Add new todo success'
+                });
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: res.message
+                });
+            }
+        })
+    }
+
+    const toggleTodo = async (id) => {
+        await fetch(`${api}/${id}/toggle`, {
+            method: 'PATCH',
+            headers: {
+                'Authorization': sessionStorage.getItem('token'),
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.status) {
+                getTodo();
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Success',
+                    text: 'Todo status updated'
+                });
+            }
+        })
+        .catch((err) => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: err.message
+            });
+        });
+    }
+
+    const deleteTodo = (id) => {
+        fetch(`${api}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': sessionStorage.getItem('token'),
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then(() => {
+            getTodo();
+        })
+        .catch((err) => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: err.message
+            });
+        });
+    }
+
+    const setActiveTab = (tab) => {
+        activeTab.value = tab;
+    }
+
+    const filteredTodoList = computed(() => {
+        if (activeTab.value === 'incompleted') {
+            return todoList.value.filter((todo) => todo.status === false);
+        } else if (activeTab.value === 'completed') {
+            return todoList.value.filter((todo) => todo.status === true);
+        } else {
+            return todoList.value;
+        }
+    });
+
+    const finishTodo = computed(() => {
+        return todoList.value.filter((todo) => todo.status === true).length;
+    });
+
+    onMounted(() => {
+        getTodo();
+    });
+</script>
