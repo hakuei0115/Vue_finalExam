a54fbed1083567da608e8f9869c83412c2be7410
hakuei0115 <king20010115@gmail.com>
2024-08-29 00:02:19 +0800

    chore: Add VeeValidate configuration and components
    新增  VeeValidate 驗證設定和元件

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate';
+import * as AllRules from '@vee-validate/rules';
+import { localize, setLocale } from '@vee-validate/i18n';
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
 import App from './App.vue'
 import router from './router'
 
+Object.keys(AllRules).forEach(rule => {
+    if (typeof AllRules[rule] === 'function') {
+        defineRule(rule, AllRules[rule]);
+    }
+});
+
+configure({
+    generateMessage: localize({ zh_TW: zhTW }),
+    validateOnInput: true,
+});
+setLocale('zh_TW');
+
 const app = createApp(App)
+
+app.component('VField', Field);
+app.component('VForm', Form);
+app.component('ErrorMessage', ErrorMessage);
 
 app.use(router)
 
--- src/views/UserLogin.vue
+++ src/views/UserLogin.vue
@@ -6,49 +6,36 @@
         <img class="d-m-n" src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/img.png" alt="workImg">
       </div>
       <div>
-        <form class="formControls">
+        <VForm class="formControls" v-slot="{ meta }" @submit="login">
           <h2 class="formControls_txt">最實用的線上代辦事項服務</h2>
           <label class="formControls_label" for="email">Email</label>
-          <input class="formControls_input" type="text" name="email" placeholder="請輸入 email" required v-model="username">
-          <span v-if="!usernameIsNotEmpty">此欄位不可留空</span>
+          <VField class="formControls_input" type="text" name="email" placeholder="請輸入 email" rules="required|email"></VField>
+          <ErrorMessage name="email"/>
           <label class="formControls_label" for="pwd">密碼</label>
-          <input class="formControls_input" type="password" name="pwd" placeholder="請輸入密碼" required v-model="password">
-          <input class="formControls_btnSubmit" type="button" value="登入" @click="login">
-          <router-link to="/register">
-            <a class="formControls_btnLink">註冊帳號</a>
-          </router-link>
-        </form>
+          <VField class="formControls_input" type="password" name="pwd" placeholder="請輸入密碼" rules="required|min:8"></VField>
+          <ErrorMessage name="pwd"/>
+          <button class="formControls_btnSubmit" type="submit" :disabled="!meta.valid">登入</button>
+        </VForm>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-  import { ref, computed } from 'vue'
   import { useRouter } from 'vue-router'
   import showAlert from '@/utils/showAlert';
 
   const router = useRouter()
 
-  const username = ref('');
-  const password = ref('');
-
-  const usernameIsNotEmpty = computed(() => username.value !== '');
-
-  const login = async () => {
-    if (username.value === '' || password.value === '') {
-      showAlert('error', 'Oops...', '請輸入帳號密碼', 'OK');
-      return;
-    }
-
+  const login = async (value) => {
     await fetch('https://todolist-api.hexschool.io/users/sign_in', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        email: username.value,
-        password: password.value,
+        email: value.email,
+        password: value.pwd,
       }),
     })
       .then((response) => response.json())
